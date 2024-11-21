dcf098fa3675613830d078540590aab924df83e6
mariberg <marika.bergman@gmail.com>
2024-11-21 14:20:22 +0000

    add global timeouts due to new cypress version

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,9 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl: "https://x9yxbl5ntc.execute-api.eu-west-2.amazonaws.com/prod/",
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000
   },
   video: false,
   screenshotOnRunFailure: false
